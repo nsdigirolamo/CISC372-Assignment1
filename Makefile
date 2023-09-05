bff167bb977b0d1f24134d7d10d1ff49a1b22df3
nick <nsdigirolamo@gmail.com>
2023-09-05 18:48:28 -0400

    Hello world program.

---
+++ Makefile
@@ -0,0 +1,7 @@
+CC = gcc
+
+make:
+	$(CC) -o hw1 hw1.c
+
+clean:
+	rm hw1
---
+++ hw1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define N 5
+#define M 6
+
+int main (int argc, char** argv) {
+    // matrixArrayOfArrays(N, M);
+    // matrixOneBigArray(N, M);
+    printf("Hello world.\n");
+    return 0;
+}
