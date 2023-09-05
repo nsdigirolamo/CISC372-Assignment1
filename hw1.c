64b5174f7ce711b83b20018d57f721f49710906f
nick <nsdigirolamo@gmail.com>
2023-09-05 19:19:25 -0400

    Completed first function.

--- hw1.c
+++ hw1.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define N 5
 #define M 6
 
+void matrixArrayOfArrays (int n, int m) {
+
+    float** my_array = malloc(m * sizeof(float*));
+
+    for (int i = 0; i < m; i++) {
+        my_array[i] = malloc(n * sizeof(float));
+    }
+
+    int count = 1;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            my_array[i][j] = count++;
+            printf("%f\t", my_array[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < m; i++) {
+        free(my_array[i]);
+    }
+
+    free(my_array);
+
+    return;
+}
+
+void matrixOneBigArray (int n, int m) {
+    return;
+}
+
 int main (int argc, char** argv) {
-    // matrixArrayOfArrays(N, M);
-    // matrixOneBigArray(N, M);
-    printf("Hello world.\n");
+    matrixArrayOfArrays(N, M);
+    matrixOneBigArray(N, M);
     return 0;
 }
