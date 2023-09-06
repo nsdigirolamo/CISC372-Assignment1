3767dd8978cf3e007234d74e3f634795663d46c2
nick <nsdigirolamo@gmail.com>
2023-09-06 09:31:39 -0400

    Completed second function.

--- hw1.c
+++ hw1.c
@@ -30,11 +30,34 @@ void matrixArrayOfArrays (int n, int m) {
 }
 
 void matrixOneBigArray (int n, int m) {
+
+    float** ptr_array = malloc(m * sizeof(float*));
+    float* num_array = malloc(m * n * sizeof(float));
+
+    for (int i = 0; i < m; i++) {
+        ptr_array[i] = &num_array[i * n];
+    }
+
+    for (int j = 0; j < m * n; j++) {
+        num_array[j] = j + 1;
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%f\t", ptr_array[i][j]);
+        }
+        printf("\n");
+    }
+
+    free(ptr_array);
+    free(num_array);
+
     return;
 }
 
 int main (int argc, char** argv) {
     matrixArrayOfArrays(N, M);
+    printf("\n");
     matrixOneBigArray(N, M);
     return 0;
 }
